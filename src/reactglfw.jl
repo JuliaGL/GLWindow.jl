afa73eb520a826ef29ce00eac550184c7bc290f5
Simon Danisch <sdanisch@gmail.com>
2014-06-18 21:40:31 +0200

    added debugg callback

--- src/reactglfw.jl
+++ src/reactglfw.jl
@@ -139,6 +139,33 @@ function renderloop(window)
 	end
 	GLFW.Terminate()
 end
+function openglerrorcallback(
+				source::GLenum, typ::GLenum,
+				id::GLuint, severity::GLenum,
+				length::GLsizei, message::Ptr{GLchar},
+				userParam::Ptr{Void}
+			)
+	errormessage = 	"\n"*
+					" ________________________________________________________________\n"* 
+					"|\n"*
+					"| OpenGL Error!\n"*
+					"| source: $(GLENUM(source).name) :: type: $(GLENUM(typ).name)\n"*
+					"| "*ascii(bytestring(message, length))*"\n"*
+					"|________________________________________________________________\n"
+
+	if typ == GL_DEBUG_TYPE_PERFORMANCE
+		println(errormessage)
+	else
+		error(errormessage)
+	end
+	nothing
+end
+
+global const _openglerrorcallback = cfunction(openglerrorcallback, Void,
+										(GLenum, GLenum,
+										GLuint, GLenum,
+										GLsizei, Ptr{GLchar},
+										Ptr{Void}))
 
 function createWindow(name::String, w, h; debugging = true)
 	GLFW.Init()
