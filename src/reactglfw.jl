396959b6d2546bba331f46a9ccad1dae72ac3b56
SimonDanisch <sdanisch@gmail.com>
2014-09-15 14:43:17 +0200

    removed wrong mouse position bug

--- src/reactglfw.jl
+++ src/reactglfw.jl
@@ -269,7 +269,7 @@ function createwindow(name::String, w, h; debugging = false, windowhints=[(GLFW.
 	glViewport(0, 0, width, height)
 
 	mouseposition_glfw 	= Input(Vector2(0.0))
-	mouseposition 		= lift((mouse, window) -> Vector2(mouse[1], window[2] - mouse[2]), Vector2{Float64}, mouseposition_glfw, window_size)
+	mouseposition 		= lift((mouse, window) -> Vector2(mouse[1], window[4] - mouse[2]), Vector2{Float64}, mouseposition_glfw, window_size)
 
 	
 	inputs = [
