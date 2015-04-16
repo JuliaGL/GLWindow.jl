b312ba27f0145a034fed8a8a7ca841938b74be4a
Simon Danisch <sdanisch@gmail.com>
2015-04-16 14:26:47 +0200

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,13 @@ using GLWindow, GLFW
 using Base.Test  
 
 # write your own tests here
-window = createwindow("test", 10,10)
+window = createwindow("test", 512,512)
 
-GLFW.SwapBuffers(window.glfwWindow)
-GLFW.PollEvents()
-
+GLFW.SwapBuffers(window.nativewindow)
+while window.inputs[:open].value
+	yield()
+	GLFW.PollEvents()
+end
 GLFW.Terminate()
 
 
