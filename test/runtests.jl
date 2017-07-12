8eeb5218f19183dfea1873b036d499763a767310
SimonDanisch <sdanisch@gmail.com>
2017-07-12 13:20:47 +0200

    checkout GLAbstraction

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,19 @@
+function is_ci()
+    get(ENV, "TRAVIS", "") == "true" ||
+    get(ENV, "APPVEYOR", "") == "true" ||
+    get(ENV, "CI", "") == "true"
+end
+if is_ci()
+    cd(Pkg.dir("GLAbstraction")) do
+        run(`git fetch origin`)
+        run(`git checkout master`)
+    end
+end
+
 using GLWindow
 using Base.Test
 
-function is_ci()
-	get(ENV, "TRAVIS", "") == "true" || get(ENV, "APPVEYOR", "") == "true" || get(ENV, "CI", "") == "true"
-end
+
 
 if !is_ci() # only do test if not CI... this is for automated testing environments which fail for OpenGL stuff, but I'd like to test if at least including works
 
