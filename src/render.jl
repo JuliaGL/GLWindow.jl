22987978c85c486f9d85fd14f1f5891b8f6315ae
Simon <sdanisch@gmail.com>
2017-08-08 16:26:39 +0200

    Tsur herman global prerender new pr (#47)
    * global prerender, avoid using slow rational number for limiting fps, removed problematic Reactive.run_till_now
    
    *  timer_pessimistic to use cheap time() instead of somewhat slower tic and toc. added a safe version of Reactive.run_till_now()
    
    * cleanup
    
    * allow nightly to fail

--- src/render.jl
+++ src/render.jl
@@ -19,49 +19,54 @@ Sleep is pretty imprecise. E.g. anything under `0.001s` is not guaranteed to wak
 up before `0.001s`. So this timer is pessimistic in the way, that it will never
 sleep more than `time`.
 """
-@inline function sleep_pessimistic(time)
-    while time >= 0.001
-        tic()
+@inline function sleep_pessimistic(sleep_time)
+    st = convert(Float64,sleep_time) - 0.002
+    start_time = time()
+    while (time() - start_time) < st
         sleep(0.001) # sleep for the minimal amount of time
-        time -= toq()
     end
 end
-function poll_reactive()
-    # run_till_now blocks when message queue is empty!
-    Base.n_avail(Reactive._messages) > 0 && Reactive.run_till_now()
+function reactive_run_till_now()
+    max_yield = Base.n_avail(Reactive._messages) * 2
+    for i=1:max_yield
+        if !isready(Reactive._messages)
+            break
+        end
+        yield()
+    end
 end
-function renderloop(window::Screen, framerate = 1//60)
+function renderloop(window::Screen; framerate = 1/60,
+    prerender = () -> nothing)
     while isopen(window)
-        tic()
+        t = time()
+        prerender()
         render_frame(window)
         swapbuffers(window)
         poll_glfw()
         yield()
-        sleep_pessimistic(framerate - toq()) #
+        sleep_pessimistic(framerate - (time() - t)) #
     end
     destroy!(window)
     return
 end
 
-import GLWindow: poll_reactive, sleep_pessimistic
 
-function waiting_renderloop(screen, framerate = 1//60)
-    Reactive.stop()
+function waiting_renderloop(screen; framerate = 1/60,
+    prerender = () -> nothing)
+
     while isopen(screen)
-        tic()
+        t = time()
         poll_glfw() # GLFW poll
+        prerender()
         if Base.n_avail(Reactive._messages) > 0
-            poll_reactive() # reactive poll
-            poll_reactive() # two times for secondary signals
+            reactive_run_till_now()
             render_frame(screen)
             swapbuffers(screen)
-            yield() # yield in timings? Seems fair
         end
-        t = toq()
-        sleep_pessimistic((1/60) - t)
+        t = time() - t
+        sleep_pessimistic(framerate - t)
     end
     destroy!(screen)
-    @async Reactive.run() #turn reactive back on
     return
 end
 
